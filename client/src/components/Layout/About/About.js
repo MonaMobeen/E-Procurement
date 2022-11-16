0d7d3ab770e1389da7ea8559bc61491ea5ffa79c
MemoonaMobeen <memoonamobeen618@gmail.com>
2022-11-16 16:54:45 +0500

    action fixed for client

---
+++ client/src/components/Layout/About/About.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./aboutSection.css";
+import { Button, Typography, Avatar } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import InstagramIcon from "@material-ui/icons/Instagram";
+const About = () => {
+  const visitInstagram = () => {
+    window.location = "https://instagram.com/meabhisingh";
+  };
+  return (
+    <div className="aboutSection">
+      <div></div>
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
+
+        <div>
+          <div>
+            <Avatar
+              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+              src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fnature%2F&psig=AOvVaw3r5DklR6HHnaw7cuDI1t5J&ust=1668509581684000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCKDP5tnArfsCFQAAAAAdAAAAABAO"
+              alt="Founder"
+            />
+            <Typography>Memoona Mobeen</Typography>
+            <Button onClick={visitInstagram} color="primary">
+              Visit Instagram
+            </Button>
+            <span>
+              This is a sample wesbite made by @memoonamobeen. 
+            </span>
+          </div>
+          <div className="aboutSectionContainer2">
+            <Typography component="h2">Our Brands</Typography>
+            <a
+              href="https://www.youtube.com/channel/UCO7afj9AUo0zV69pqEYhcjw"
+              target="blank"
+            >
+              <YouTubeIcon className="youtubeSvgIcon" />
+            </a>
+
+            <a href="https://instagram.com/meabhisingh" target="blank">
+              <InstagramIcon className="instagramSvgIcon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
